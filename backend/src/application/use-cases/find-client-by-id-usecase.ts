4a257a43cc3a1ee4791e6cc66c2377061814f3b6
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2025-06-03 13:17:50 -0300

    feat: add allocation retrieval to FindClientByIdUseCase and update AllocationRepository interface

--- backend/src/application/use-cases/find-client-by-id-usecase.ts
+++ backend/src/application/use-cases/find-client-by-id-usecase.ts
@@ -1,4 +1,6 @@
+import { Allocation } from "../../domain/allocation";
 import { Client } from "../../domain/client";
+import { AllocationRepository } from "../../domain/repositories/allocation-repository";
 import { ClientRepository } from "../../domain/repositories/client-repository";
 import { logger } from "../../infra/http/logger";
 
@@ -8,10 +10,14 @@ export type FindClientByIdInputDTO = {
 
 export type FindClientByIdOutputDTO = {
   client: Client | null;
+  allocations?: Allocation[];
 };
 
 export class FindClientByIdUseCase {
-  constructor(private readonly clientRepository: ClientRepository) {}
+  constructor(
+    private readonly clientRepository: ClientRepository,
+    private readonly allocationRepository: AllocationRepository
+  ) {}
 
   async execute(
     input: FindClientByIdInputDTO
@@ -25,9 +31,10 @@ export class FindClientByIdUseCase {
 
     const output: FindClientByIdOutputDTO = {
       client: await this.clientRepository.findById(input.id),
+      allocations: await this.allocationRepository.findByClientId(input.id),
     };
 
-    if (!output.client) {
+    if (!output.client && !output.allocations?.length) {
       logger.warn({
         code: "FIND_CLIENT_BY_ID_NOT_FOUND",
         message: `Client not found with id: ${input.id}`,
--- backend/src/domain/repositories/allocation-repository.ts
+++ backend/src/domain/repositories/allocation-repository.ts
@@ -3,5 +3,4 @@ import { Allocation } from "../allocation";
 export interface AllocationRepository {
   create(allocation: Allocation): Promise<Allocation>;
   findByClientId(clientId: string): Promise<Allocation[]>;
-  deleteByClientId(clientId: string): Promise<void>;
 }

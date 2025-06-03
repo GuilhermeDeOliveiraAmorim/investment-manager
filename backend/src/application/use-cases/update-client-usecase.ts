8c7edb389fb1ef61abcf55bb2b77905fdc011dc4
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2025-06-03 09:53:58 -0300

    feat: implement UpdateClient use case with error handling and logging

---
+++ backend/src/application/use-cases/update-client-usecase.ts
@@ -0,0 +1,66 @@
+import { Client, ClientStatus } from "../../domain/client";
+import { ClientRepository } from "../../domain/repositories/client-repository";
+import { logger } from "../../infra/http/logger";
+
+export type UpdateClientInputDTO = {
+  id: string;
+  name: string;
+  email: string;
+  status: ClientStatus;
+};
+
+export type UpdateClientOutputDTO = {
+  client: Client;
+};
+
+export class UpdateClientUseCase {
+  constructor(private readonly clientRepository: ClientRepository) {}
+
+  async execute(input: UpdateClientInputDTO): Promise<UpdateClientOutputDTO> {
+    logger.info({
+      code: "UPDATE_CLIENT",
+      message: "Updating client",
+      layer: "usecase",
+      meta: {
+        clientId: input.id,
+        timestamp: new Date().toISOString(),
+      },
+    });
+
+    const client = await this.clientRepository.findById(input.id);
+
+    if (!client) {
+      logger.warn({
+        code: "CLIENT_NOT_FOUND",
+        message: "Client not found",
+        layer: "usecase",
+        meta: {
+          clientId: input.id,
+          timestamp: new Date().toISOString(),
+        },
+      });
+
+      throw new Error("Client not found");
+    }
+
+    client.update(input.name, input.email, input.status);
+
+    const output: UpdateClientOutputDTO = {
+      client: await this.clientRepository.update(client),
+    };
+
+    logger.info({
+      code: "UPDATE_CLIENT_SUCCESS",
+      message: "Client updated successfully",
+      layer: "usecase",
+      meta: {
+        clientId: output.client.id,
+        timestamp: new Date().toISOString(),
+      },
+    });
+
+    return {
+      client: output.client,
+    };
+  }
+}
--- backend/src/domain/repositories/client-repository.ts
+++ backend/src/domain/repositories/client-repository.ts
@@ -2,7 +2,7 @@ import { Client } from "../client";
 
 export interface ClientRepository {
   create(client: Client): Promise<Client>;
-  update(client: Client): Promise<void>;
+  update(client: Client): Promise<Client>;
   findById(id: string): Promise<Client | null>;
   findAll(): Promise<Client[]>;
 }
